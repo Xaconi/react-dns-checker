c98dea408deaff75e8e3adac0385559e732db561
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2020-10-13 23:15:48 +0200

    Added all DNS records to the Netlify function call

--- functions/dns-check.js
+++ functions/dns-check.js
@@ -15,13 +15,29 @@ exports.handler = async event => {
 	const domain = event.queryStringParameters.domain;
 
 	const { address, family } = await dnsLookUp(domain);
+	const recordsA = await getDNSRecord(domain, type = 'A');
+	const recordsAAAA = await getDNSRecord(domain, type = 'AAAA');
+	const recordsMX = await getDNSRecord(domain, type = 'MX');
+	const recordsCNAME = await getDNSRecord(domain, type = 'CNAME');
+	const recordsNS = await getDNSRecord(domain, type = 'NS');
+	const recordsPTR = await getDNSRecord(domain, type = 'PTR');
+	const recordsSOA = await getDNSRecord(domain, type = 'SOA');
+	const recordsTXT = await getDNSRecord(domain, type = 'TXT');
 
 	return {
 		statusCode: 200,
 		headers,
 		body: JSON.stringify({
 			address,
-			family
+			family,
+			recordsA,
+			recordsAAAA,
+			recordsMX,
+			recordsCNAME,
+			recordsNS,
+			recordsPTR,
+			recordsSOA,
+			recordsTXT
 		})
 	}
 }
@@ -29,8 +45,15 @@ exports.handler = async event => {
 async function dnsLookUp(domain){
     return new Promise((resolve, reject) => {
         dns.lookup(domain, (err, address, family) => {
-			if(err) reject(err);
             resolve({ address, family });
         });
    });
 };
+
+async function getDNSRecord(domain, type) {
+	return new Promise((resolve, reject) => {
+		dns.resolve(domain, type, (err, records) => {
+			resolve(records);
+		});
+	});
+}
